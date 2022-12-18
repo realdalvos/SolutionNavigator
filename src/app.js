dc39dac74c89d0f24570f7cb37ea0d8ce2c8b89f
vlad <vlad.cordos@gmail.com>
2022-12-18 03:55:32 +0100

    js for hamburger

--- src/app.js
+++ src/app.js
@@ -2,5 +2,13 @@ console.log("Welcome to Solution Navigator")
 button.addEventListener("click", newsletterSubscribe);
 
 function newsletterSubscribe(){
-
 }
+
+const hamburgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
+
+function toggleButton(){
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
