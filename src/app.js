5fd10e126be83b698d3d41d1bf5448ac04849613
vlad <vlad.cordos@yahoo.com>
2023-01-02 19:30:13 +0100

    Add hamburger sidebar(menu), not working currently

--- src/app.js
+++ src/app.js
@@ -1,4 +1,22 @@
 console.log("Welcome to Solution Navigator")
+
+let countValue = new Promise(function (resolve, reject) {
+   reject('Promise rejected');
+});
+
+// executes when promise is resolved successfully
+countValue.then(
+    function successValue(result) {
+        console.log(result);
+    },
+ )
+
+// executes if there is an error
+.catch(
+    function errorValue(result) {
+        console.log(result);
+    }
+);
 button.addEventListener("click", newsletterSubscribe);
 
 function newsletterSubscribe(){
