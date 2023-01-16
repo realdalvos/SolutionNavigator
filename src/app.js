76d9503c20835ec78def9622ee740354321e78bb
vlad <vlad.cordos@gmail.com>
2023-01-16 04:04:06 +0100

    flex and other slight changes

--- src/app.js
+++ src/app.js
@@ -1,19 +1,11 @@
 console.log("Welcome to Solution Navigator")
 
 
-button.addEventListener("click", newsletterSubscribe);
 
-function newsletterSubscribe(){
-}
 
-const hamburgerButton = document.getElementById('hamburger')
-const navList = document.getElementById('nav-list')
 
-function toggleButton(){
-    navList.classList.toggle('show')
-}
 
-hamburgerButton.addEventListener('click', toggleButton)
+
 
 let countValue = new Promise(function (resolve, reject) {
    reject('Promise rejected');
