22c8f5e098c9edfd32b23ec46c9e1c2cc77635f9
vlad <vlad.cordos@gmail.com>
2023-01-17 04:10:58 +0100

    showInformation function und onclick for subjects

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ console.log("Welcome to Solution Navigator")
 
 
 
-
+function showInformation(subject){
+    const messageHTML= 'This text is for HTML';
+    const messageCSS= 'This text is for CSS';
+    const messageRWD= 'This text is for RWD';
+    console.log("it works");
+    switch (subject){
+        case 1:
+            console.log("it works");
+            document.getElementById("message").innerHTML =  messageHTML;
+            break;
+        case 2:
+            document.getElementById("message").innerHTML =  messageCSS;
+            break;
+        case 3:
+            document.getElementById("message").innerHTML =  messageRWD;
+    }
+}
 
 let countValue = new Promise(function (resolve, reject) {
    reject('Promise rejected');
