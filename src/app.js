a1e9040ab285225f4c04caf2a36a1ad0bde96a4e
vlad <vlad.cordos@gmail.com>
2023-01-18 20:00:14 +0100

    Smileys

--- src/app.js
+++ src/app.js
@@ -48,3 +48,38 @@ countValue.then(
         console.log(result);
     }
 );
+
+
+//Uebung 4.2
+const Person = {
+    cars: []
+};
+const Auto = {};
+
+const ford = {
+    owner: '',
+    __proto__: Person
+};
+const seat = {
+    owner: '',
+    __proto__: Person
+};
+
+const hans = {
+    cars: [seat],
+    __proto__: Person
+};
+
+const peter = {
+    cars: [ford,seat],
+    __proto__: Person
+};
+
+function conflict(car, persons){
+    var owners = 0;
+    persons.forEach( person => person.cars.include( car ) &&  owners++);
+    return owners > 1;
+}
+
+const result = conflict(seat, [peter, hans ]);
+console.log(result);
