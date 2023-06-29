9207e86db6cc629ec6b4b9ec8d6b008be4874339
vlad <vlad.cordos@gmail.com>
2023-06-29 02:20:49 +0200

    solutions to JavaScript Exercises and minor changes to table references

--- src/js/functional.js
+++ src/js/functional.js
@@ -1,7 +1,7 @@
 function addf(x) {
     return function(y) {
-        return x + y;
-    };
+        return x + y
+    }
 }
 const add5 = addf(5);
 console.log(add5(3)); // 8
---
+++ src/solutions/JS.js
@@ -0,0 +1,71 @@
+function identity(parameter){
+    console.log(parameter)
+}
+
+function identity_function(parameter){
+    identity(parameter)
+}
+
+function add(x, y){
+    return x+y
+}
+
+function mul(x,y){
+    return x*y
+}
+
+function addf(x) {
+    return function(y) {
+        return x + y
+    }
+}
+
+const Person = {
+    cars: []
+};
+const Auto = {};
+
+const ford = {
+    owner: '',
+    __proto__: Person
+};
+const seat = {
+    owner: '',
+    __proto__: Person
+};
+
+const hans = {
+    cars: [seat],
+    __proto__: Person
+};
+
+const peter = {
+    cars: [ford,seat],
+    __proto__: Person
+};
+
+function conflict(car, persons){
+    var owners = 0;
+    persons.forEach( person => person.cars.include( car ) &&  owners++);
+    return owners > 1;
+}
+
+const result = conflict(seat, [peter, hans ]);
+console.log(result);
+
+let fibonnaciNumber = 0
+
+function fibonnaciUntil2000(x,y){
+    if(fibonnaciNumber=0){
+        console.log(x)
+        console.log(y)
+        fibonnaciNumber++
+        fibonnaciUntil2000(y,x+y)
+    }
+    if(fibonnaciNumber<2001){
+        console.log(x+y)
+        fibonnaciUntil2000(y,x+y)
+    }
+}
+
+fibonnaciUntil2000(0,1)
